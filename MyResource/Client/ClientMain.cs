491051a7de069b39b6a2b922d529f8435666ba14
Zulfurix <zulfurix@gmail.com>
2024-08-18 09:43:37 +0100

    Remove DrawRect()

--- MyResource/Client/ClientMain.cs
+++ MyResource/Client/ClientMain.cs
@@ -15,8 +15,6 @@ namespace MyResource.Client
         [Tick]
         public Task OnTick()
         {
-            DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
-
             return Task.FromResult(0);
         }
     }
